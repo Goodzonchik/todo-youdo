6ca6c028619a34f5ce158a02829a1122340016c2
Goodzonchik <goodzonchik@rambler.ru>
2022-07-07 08:28:45 +0500

    remove material module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material/material.module';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { GridListComponent } from './components/grid-list/grid-list.component';
 import { ArraySortLengthPipe } from './pipes/sort-by-length.pipe';
 import { ArraySortFieldPipe } from './pipes/sort-by-field.pipe';
+
+const materialModules = [
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatListModule,
+  MatDialogModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatGridListModule,
+  MatCardModule,
+  MatSnackBarModule,
+];
 
 @NgModule({
   declarations: [
@@ -18,12 +48,7 @@ import { ArraySortFieldPipe } from './pipes/sort-by-field.pipe';
     ArraySortFieldPipe,
     ArraySortLengthPipe,
   ],
-  imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule],
-  exports: [
-    ToolbarComponent,
-    DialogComponent,
-    CardComponent,
-    GridListComponent,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, materialModules],
+  exports: [ToolbarComponent, DialogComponent, CardComponent, GridListComponent],
 })
 export class SharedModule {}
